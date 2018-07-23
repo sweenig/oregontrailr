a4f073a4811de56d70b52ceb9525f864f1e88326
sweenig <sweenig@users.noreply.github.com>
2018-07-23 15:14:36 -0500

    New save option integrated with form

---
+++ catalog.js
@@ -0,0 +1,121 @@
+trailers = {
+  terradrop: {
+    name: "TerraDrop",
+    price: 11000,
+    img: "trailers/terradrop.jpg",
+    tagline: "A teardrop trailer built to stand up to the rigors of the Off-Road!",
+    description: "TerraDrop is our offering for those of you with off-road tendencies. TerraDrop is styled to fit in perfectly with modern and classic off-road capable vehicles. It has adventure in its DNA! While maintaining the exact same overall size as FronTear, TerraDrop manages to offer even more internal volume. So you get more storage space and more headroom to boot. TerraDrop can be configured from mild to wild, so it can be made to suite your needs and budget.",
+  },
+  terradropalpha: {
+    name: "TerraDrop Alpha",
+    price: 12000,
+    img: "trailers/terradrop.alpha.jpg",
+    tagline: "",
+    description: "<span style='font-weight:bold;'>TerraDrop <span style='color:#8d2424;'>ALPHA</span></span> is the next level of evolution in off-road capable teardrop trailers. We've taken the already very capable and well-appointed <a href='http://www.oregontrailer.net/terradrop.html'>TerraDrop</a> platform, and made it even better with some key improvements to the the design and implementation. Featuring our super durable ALPHA coating, the exterior is FAR more durable than a traditionally built aluminum clad teardrop. Our ALPHA coating allows us to offer a completely seamless exterior for the <span style='text-decoration:underline;'>BEST</span> in waterproofing and resistance to damage. Add to that a large helping of included Off-Road specific features, and you get TerraDrop <span style='color:#8d2424;'>ALPHA</span> - <span style='font-weight:bold;'>Unmatched in its class!</span>",
+  },
+  dodrop: {
+    name: "Do-Drop",
+    price: 14000,
+    img: "trailers/do-drop.jpg",
+    tagline: "Go <span style='text-decoration:underline;'>DO</span> Stuff! DO-Drop Is Very Simple, But Has Everything You Need.",
+    description: "Do-Drop is simple, light, and affordable, but has everything you need. It can be outfitted in a variety of ways. This is the perfect camp trailer for those looking for a simple solution to get out camping, but don't have a large tow vehicle to do it with. With a bed length of 78&quot; and an interior width of 47&quot;, DoDrop is perfect for a single occupant, or a cozy couple. When equipped with a chuck-box/camp-kitchen, DoDrop delivers everything you need.",
+  },
+  dodropalpha: {
+    name: "Do-Drop Alpha",
+    price: 16000,
+    price_f: "$16k",
+    img: "trailers/dodrop.alpha.jpg",
+    tagline: "",
+    description: "<span style='font-weight:bold;'>DoDrop <span style='color:#8d2424;'>ALPHA</span></span>, (<span style='font-weight:bold;'>DD<span style='color:#8d2424;'>A</span></span> for short) is the MOST off-road capable trailer we make, which says a <span style='text-decoration:underline;'>LOT</span>. Like our other off-road capable trailers, DDA is outfitted with long-travel, fully-independent, reliable suspension, and a re-enforced ultra-strong chassis. DDA's extra capability is primarily due to it's smaller size, lighter weight, and simpler configuration. DoDrop <span style='color:#8d2424;'>ALPHA</span> will easily fit inside of pretty much any tow vehicle's track-width envelope. <span style='font-weight:bold;'>If your rig fits, your trailer will fit too!</span>",
+  },
+  frontear: {
+    name: "FronTear",
+    price: 13000,
+    img: "trailers/frontear.jpg",
+    tagline: "Oregon Trail'R Frontear  - Our flagship classic teardrop",
+    description: "A mix of classic teardrop styling with elegant modern flare make FronTear look fantastic behind any tow vehicle.  With dozens of options available, FronTear can be configured to suit your needs and desires.",
+  },
+  frontearoffroad: {
+    name: "FronTear Offroad",
+    price: 15000,
+    price_f: "$15k",
+    img: "trailers/frontear.offroad.jpg",
+    tagline: "",
+    description: "[description needed]",
+  },
+};
+options = {
+  category1: {
+    label: "Category",
+    title: "Chassis",
+  },
+  opt1: {
+    label: "15&quot; Alloy Wheels",
+    description: "<p>Standard:</p><img src='options/standard.cabinet.jpg' /><p>These add to the aesthetics and the ride quality of the teardrop</p><img src='options/custom.cabinet.jpg' />",
+    price: 149,
+  },
+  opt2: {
+    label: "Heavy Duty Trailer Tires",
+    description: "These tires won't get flat spots even after long periods of storage. Pair",
+    price: 75,
+  },
+  opt3: {
+    label: "Adjustable/Removable Tongue",
+    description: "Allows multiple towing options and extra security when leaving the teardrop",
+    price: 129,
+  },
+  opt4: {
+    label: "Rear Leveling Jacks",
+    description: "Heavy duty long extension scissor jacks get mounted under the chassis. Pair",
+    price: 289,
+  },
+  opt5: {
+    label: "Accessory Hitch",
+    description: "For mounting a bike rack or other accessories (not to exceed 150 lb load)",
+    price: 199,
+  },
+  opt6: {
+    label: "Off Road Suspension Package",
+    description: "Timbren Axless independent suspension with chassis reinforcement",
+    price: 999,
+  },
+  opt7: {
+    label: "Multi Axis MAX Coupler",
+    description: "for hard core towing situations and all around improved towing characteristics",
+    price: 299,
+  },
+  category2: {
+    label: "Category",
+    title: "Tires",
+  },
+  opt8: {
+    label: "Off Road Tires",
+    description: "Larger/meatier tires for increased grip in difficult situations",
+    price: 229,
+  },
+  opt9: {
+    label: "Fender",
+    description: "deflector/steps",
+    price: 399,
+  },
+  opt10: {
+    label: "Tongue Rack",
+    description: "Provides exterior storage in front of the teardrop",
+    price: 299,
+  },
+  opt11: {
+    label: "Spare tire",
+    description: "small doughnut style wheel and tire mounted below chassis",
+    price: 149,
+  },
+  opt12: {
+    label: "Back-up lights",
+    description: "High power LED flood light mounted under chassis",
+    price: 129,
+  },
+  opt13: {
+    label: "Electric Braking Option",
+    description: "description needed",
+    price: 479,
+  },
+}
---
+++ functions.js
@@ -0,0 +1,137 @@
+Number.prototype.formatMoney = function(c, d, t){
+    var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+function update(){
+  var inputs = document.getElementsByTagName('input'),sum = 0,builturl = '';
+  builturl = '?'; //start building the save url
+  basepriceval = document.getElementById('baseprice').value; //get the id of the trailer selected
+  builturl += 'basemodel=' + basepriceval; //add the trailer to the url as a parameter
+  for(var i=0; i<inputs.length; i++) { //loop through the options
+    var ip = inputs[i]; //current input
+    if (ip.getAttribute("class") == "w3-check" && ip.checked) { //if it's a checkbox and it's checked
+      sum += Number(ip.getAttribute("price")); //add the option's price to the sum
+      builturl += "&" + ip.getAttribute("id"); //add the option to the url
+    }
+  }
+  savebuttons = document.getElementsByClassName("savebutton"); //get the dynamic tag in each save button
+  if (basepriceval != '') { //if a base model has been selected (manually or via url)
+    email_link_suffix = 
+    "?subject=" + encodeURIComponent("Question about my ") + trailers[basepriceval].name + " build" //subject line
+    + "&body="
+    + encodeURIComponent("Here is the link to the options I've selected: ") //introductory text
+    + encodeURIComponent(location.pathname + builturl) //include the url
+    + "%0D%0A%0D%0A" //two carriage returns
+    + encodeURIComponent("Could you help me?"); //setup the email body
+    document.getElementById("mailjon").href += email_link_suffix; //append the email body to the email links
+    document.getElementById("mailsaw").href += email_link_suffix; //append the email body to the email links
+    sum += Number(trailers[basepriceval].price); //add the price of the trailer to the running total
+    document.getElementById("baseecho").innerHTML = "$" + trailers[basepriceval].price; //put the trailer price on the trailer line
+    document.getElementById("basedesc").innerHTML = trailers[basepriceval].description; //put the trailer description on the trailer line
+    document.getElementById("feature").src = trailers[basepriceval].img; //change the image to the currently selected trailer
+    for(var i=0; i<savebuttons.length;i++){ //loop through the save buttons' dynamic tags
+      savebuttons[i].innerHTML = "<i class='fa fa-sign-out w3-margin-right'></i> Request a quote for this " + trailers[basepriceval].name + " (est. $" + sum.formatMoney(0) + ")";
+      savebuttons[i].href = "https://docs.google.com/forms/d/e/1FAIpQLSebnw7jfCzKQzIo8ZuF5-NmY_FMf01sFzl5oOdUFzS2dGucTw/viewform?entry.648048562=" + encodeURIComponent(builturl);
+    }
+  } else {
+    for(var i=0; i<savebuttons.length;i++) {
+      savebuttons[i].innerHTML = "<i class='fa fa-sign-out w3-margin-right'></i> Request a quote (est. $" + sum.formatMoney(0) + ")";
+      savebuttons[i].href = "https://docs.google.com/forms/d/e/1FAIpQLSebnw7jfCzKQzIo8ZuF5-NmY_FMf01sFzl5oOdUFzS2dGucTw/viewform?entry.648048562=" + encodeURIComponent(builturl);
+    }
+  }
+  var table = document.getElementById("buildtable");
+  if (document.getElementById('totalrow') == null){ //if the total row doesn't exist, create it
+    var txt1 = document.createTextNode("Total:");
+    var td1 = document.createElement("td");
+    td1.appendChild(txt1);
+    var txt2 = document.createTextNode("");
+    var td2 = document.createElement("td");
+    td2.id = "total";
+    td2.colSpan = "2";
+    td2.appendChild(txt2);
+    var tr = document.createElement("tr");
+    tr.id = "totalrow";
+    tr.appendChild(td1);
+    tr.appendChild(td2)
+    table.appendChild(tr);
+  }
+  document.getElementById("total").innerHTML = "$" + sum.formatMoney(0); //add the total to the bottom line
+}
+function getURLParams(){
+  var parenturl=parent.document.location.search; //get the parent page URL parameters
+  if (parenturl.length > 0) {
+    parenturl=parenturl.replace('?',''); //remove the leading ? from the URL parameters
+    var argsarray=parenturl.split('&'); //split the parameters into an array of parameter pairs
+    var argsmatrix = new Array(argsarray.length); //create the matrix to hold the parameters after splitting
+    for (var i = 0; i < argsarray.length; i++){ //split the array members and put into the matrix
+      argsmatrix[i] = new Array(2); //create a new sub-array
+      argsmatrix[i] = argsarray[i].split('='); //split each row of the array into two components, store into new array as sub-array
+    }
+    return argsmatrix;
+  } else {return new Array(0);}
+}
+function initialize(){
+  for (var j = 0; j < Object.keys(trailers).length; j++) {
+    key = Object.keys(trailers)[j];
+    document.getElementById("baseprice").innerHTML += "<option value='" + key + "'>" + trailers[key].name + " - $" + trailers[key].price + "</option>";
+  }
+  var table = document.getElementById("buildtable");
+  for (var j = 0; j < Object.keys(options).length; j++) {
+    key = Object.keys(options)[j];
+    if (options[key].label == "Category"){
+      var category_header = document.createElement("h3");
+      category_header.innerHTML = options[key].title;
+      var td = document.createElement("td");
+      td.appendChild(category_header);
+      td.setAttribute("colspan","3");
+      var tr = document.createElement("tr");
+      tr.appendChild(td);
+      table.appendChild(tr);
+    } else {
+      var description_div = document.createElement("div");//create description div
+      description_div.innerHTML = options[key].description; //populate description div
+      description_div.classList.add("w3-cell-row", "w3-hide"); //add styling to description div
+      var icon = document.createElement("i"); //create icon to contain description div
+      icon.classList.add("fa", "fa-info-circle", "w3-text-gray", "w3-text-left");
+      icon.onclick = function(){this.children[0].classList.toggle('w3-hide');};
+      icon.appendChild(description_div); //add description div to icon
+      var title = document.createElement("span"); //create the label span
+      title.innerHTML = options[key].label;
+      var label = document.createElement("label");
+      label.appendChild(title); //put the label span into the label
+      label.appendChild(icon); //put the icon into the label
+      var cell1 = document.createElement("td");
+      cell1.appendChild(label);
+      var tr = document.createElement("tr");
+      tr.appendChild(cell1);
+      var checkbox = document.createElement("input");
+      checkbox.id=key;
+      checkbox.onchange=function(){update();};
+      checkbox.classList.add("w3-check");
+      checkbox.type = "checkbox";
+      checkbox.setAttribute("price",options[key].price);
+      var cell2 = document.createElement("td");
+      cell2.style.textAlign = "center";
+      cell2.appendChild(checkbox);
+      tr.appendChild(cell2);
+      var cell3 = document.createElement("td");
+      cell3.style.textAlign = "right";
+      cell3.innerHTML = "$" + options[key].price;
+      tr.appendChild(cell3);
+      table.appendChild(tr);
+    }
+  }
+  var argsmatrix = getURLParams();
+  for (var j = 0; j < argsmatrix.length; j++){
+    if (argsmatrix[j][0] == 'basemodel'){document.getElementById('baseprice').value = argsmatrix[j][1];}
+    else {document.getElementById(argsmatrix[j][0]).checked = true;}
+  }
+  update();
+}
+
